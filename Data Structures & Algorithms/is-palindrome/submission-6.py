6650fcaddc0517698cd56cbace963641ac72904f
Danilo Corrêa <d39921082@gmail.com>
2026-07-23 02:00:14 -0300

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        collapsed = ''.join(e for e in s if e.isalnum())
+        mid = len(collapsed) // 2 
+        print(collapsed[:mid].lower())       
+
+        print(collapsed[mid:][::-1].lower())
+
+        print(collapsed[mid + 1:][::-1].lower())
+
+        if len(collapsed) % 2 == 1:
+            if collapsed[:mid].lower() == collapsed[mid + 1:][::-1].lower():
+                return True
+            else:
+                return False
+        else:
+            if collapsed[:mid].lower() == collapsed[mid:][::-1].lower():
+                return True
+            else:
+                return False
